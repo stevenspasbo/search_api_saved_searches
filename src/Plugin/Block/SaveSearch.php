ae4367f9f2091247c3ede91639001ab844a3ada6
Thomas Seidl <remus@gmx.net>
2018-02-08 11:21:48 +0100

    Use single block plugin with "type" setting instead of derivatives.

--- src/Plugin/Block/SaveSearch.php
+++ src/Plugin/Block/SaveSearch.php
@@ -3,9 +3,11 @@
 namespace Drupal\search_api_saved_searches\Plugin\Block;
 
 use Drupal\Component\Utility\Xss;
+use Drupal\Core\Annotation\Translation;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormBuilderInterface;
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\search_api\Utility\QueryHelperInterface;
@@ -16,7 +18,8 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *
  * @Block(
  *   id = "search_api_saved_searches",
- *   deriver = "Drupal\search_api_saved_searches\Plugin\Block\SaveSearchBlockDeriver"
+ *   admin_label = @Translation("Save search"),
+ *   category = @Translation("Forms"),
  * )
  */
 class SaveSearch extends BlockBase implements ContainerFactoryPluginInterface {
@@ -36,11 +39,19 @@ class SaveSearch extends BlockBase implements ContainerFactoryPluginInterface {
   protected $formBuilder;
 
   /**
+   * The query helper.
+   *
+   * @var \Drupal\search_api\Utility\QueryHelperInterface|null
+   */
+  protected $queryHelper;
+
+  /**
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
     $block = new static($configuration, $plugin_id, $plugin_definition);
 
+    $block->setStringTranslation($container->get('string_translation'));
     $block->setEntityTypeManager($container->get('entity_type.manager'));
     $block->setFormBuilder($container->get('form_builder'));
     $block->setQueryHelper($container->get('search_api.query_helper'));
@@ -95,13 +106,6 @@ class SaveSearch extends BlockBase implements ContainerFactoryPluginInterface {
   }
 
   /**
-   * The query helper.
-   *
-   * @var \Drupal\search_api\Utility\QueryHelperInterface|null
-   */
-  protected $queryHelper;
-
-  /**
    * Retrieves the query helper.
    *
    * @return \Drupal\search_api\Utility\QueryHelperInterface
@@ -127,6 +131,45 @@ class SaveSearch extends BlockBase implements ContainerFactoryPluginInterface {
   /**
    * {@inheritdoc}
    */
+  public function defaultConfiguration() {
+    return [
+      'type' => NULL,
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $types = $this->getEntityTypeManager()
+      ->getStorage('search_api_saved_search_type')
+      ->loadMultiple();
+    $type_options = [];
+    foreach ($types as $type_id => $type) {
+      $type_options[$type_id] = $type->label();
+    }
+    $form['type'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Saved search type'),
+      '#description' => $this->t('Saved search type'),
+      '#options' => $type_options,
+      '#default_value' => $this->configuration['type'],
+      '#required' => TRUE,
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $this->configuration['type'] = $form_state->getValue('type');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function access(AccountInterface $account, $return_as_object = FALSE) {
     $access = parent::access($account, TRUE);
 
@@ -145,7 +188,7 @@ class SaveSearch extends BlockBase implements ContainerFactoryPluginInterface {
     // @todo Move those checks to access()? Would mean access results can't be
     //   cached, though.
     $type = $this->getSavedSearchType();
-    if (!$type) {
+    if (!$type || !$type->status()) {
       return [];
     }
     $query = $type->getActiveQuery($this->getQueryHelper());
@@ -198,9 +241,14 @@ class SaveSearch extends BlockBase implements ContainerFactoryPluginInterface {
    *   The saved search type, or NULL if it couldn't be loaded.
    */
   protected function getSavedSearchType() {
-    return $this->getEntityTypeManager()
+    if (!$this->configuration['type']) {
+      return NULL;
+    }
+    /** @var \Drupal\search_api_saved_searches\SavedSearchTypeInterface $type */
+    $type = $this->getEntityTypeManager()
       ->getStorage('search_api_saved_search_type')
-      ->load($this->getDerivativeId());
+      ->load($this->configuration['type']);
+    return $type;
   }
 
 }
