c63d461a0a7b10a95698361cd62d041300204f36
Thomas Seidl <remus@gmx.net>
2018-03-14 16:40:20 +0100

    Fixed cache metadata for forms.

--- src/Form/SavedSearchCreateForm.php
+++ src/Form/SavedSearchCreateForm.php
@@ -19,4 +19,31 @@ class SavedSearchCreateForm extends ContentEntityForm {
     return $actions;
   }
 
+  /**
+   * {@inheritdoc}
+   */
+  public function save(array $form, FormStateInterface $form_state) {
+    $return = parent::save($form, $form_state);
+
+    if ($return === SAVED_NEW) {
+      /** @var \Drupal\search_api_saved_searches\SavedSearchInterface $search */
+      $search = $this->entity;
+      // @todo Add status field for saved searches.
+      $enabled = TRUE;
+      if ($enabled) {
+        if ($search->get('notify_interval')->value < 0) {
+          $this->messenger()->addStatus($this->t('Your saved search was successfully created.'));
+        }
+        else {
+          $this->messenger()->addStatus($this->t('Your saved search was successfully created. You will receive notifications for new results in the future.'));
+        }
+      }
+      else {
+        $this->messenger()->addStatus($this->t('Your saved search was successfully created. You will soon receive an e-mail with a confirmation link to activate it.'));
+      }
+    }
+
+    return $return;
+  }
+
 }
